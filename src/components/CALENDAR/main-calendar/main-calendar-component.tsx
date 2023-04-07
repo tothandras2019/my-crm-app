2587de7a3617eba20226fbd5aa7a319240f6014e
Andras <toth.andras.2000@gmail.com>
2023-04-07 10:47:39 +0200

    feat: calendar design and search for month implemented

--- src/components/CALENDAR/main-calendar/main-calendar-component.tsx
+++ src/components/CALENDAR/main-calendar/main-calendar-component.tsx
@@ -1,14 +1,19 @@
-import './main-calendar-component.css';
-import { useEffect, useState } from 'react';
-import CALENDAR_DATA from './../../../DATASTORE/calendar/calendar_init.json';
-import { Day } from './calendar-day/calendar-day-component';
-import { Month, MonthType } from './calendar-month/calendar-month-component';
-import { CustomButton } from '../../tools/button/submit/custom-button-component';
+import "./main-calendar-component.css";
+import { FormEvent, useEffect, useRef, useState } from "react";
+import CALENDAR_DATA from "./../../../DATASTORE/calendar/calendar_init.json";
+import { Day } from "./calendar-day/calendar-day-component";
+import { Month, MonthType } from "./calendar-month/calendar-month-component";
+import { CustomButton } from "../../tools/button/submit/custom-button-component";
+import { SearchInput } from "../../tools/search-input/search-input-component";
 
 export const MainCalendarComponent = (): JSX.Element => {
   const [months, SetMonthes] = useState<MonthType[] | null>(null);
+  const [searchedMonth, setSearchedMonth] = useState<MonthType|null>(null)
   const [weekdays, SetWeekdays] = useState<string[] | null>(null);
   const [currentYear, SetCurrentYear] = useState<number>(2023);
+  
+  const searchValue = useRef<HTMLInputElement | null>(null);
+
 
   const handle_IncreaseYear = () => {
     SetCurrentYear((state) => state + 1);
@@ -25,7 +30,12 @@ export const MainCalendarComponent = (): JSX.Element => {
     SetCurrentYear(parseInt(actualYear));
   }, []);
 
-  const calculateLeftDaysFromPrevoiusMonth = (month: MonthType, index: number, array: MonthType[]) => {
+  const calculateLeftDaysFromPrevoiusMonth = (
+    month: MonthType,
+    index: number,
+    array: MonthType[]
+  ) => {
+    console.log(index)
     const prevMonthDays = index <= 0 ? array[array.length - 1] : array[index - 1];
     const isBeforeDecember = month.nth - 1 === 0;
 
@@ -35,20 +45,50 @@ export const MainCalendarComponent = (): JSX.Element => {
     return new Date(`${actualYear}.${prevMonth}.${prevMonthDays.days}`).getDay();
   };
 
+  const submit_Search = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const current = searchValue.current;
+    if (!current) return;
+
+    const month = months?.find((monthObj) =>
+      monthObj.month.toLowerCase().includes(current.value.toLowerCase())
+    );
+
+    if (month) setSearchedMonth(() => month);
+    current.value = "";
+  };
+
+  useEffect(()=>{
+    //console.log(searchedMonth)
+  },[searchedMonth])
+
   return (
-    <div className='calendar-container'>
+    <div className="calendar-container">
       <h2>{currentYear}</h2>
-      <div className='change-years-container'>
-        <CustomButton handler={handle_DecreiseYear} value={'Last year'} color={'blue'} />
-        <CustomButton handler={handle_IncreaseYear} value={'Next year'} color={'green'} />
+      <div className="change-years-container">
+        <CustomButton handler={handle_DecreiseYear} value={"Last year"} color={"blue"} />
+        <CustomButton handler={handle_IncreaseYear} value={"Next year"} color={"green"} />
+      </div>
+      <div>
+        <form onSubmit={submit_Search}>
+          <SearchInput searchedValueRef={searchValue} reset={()=>{}} />
+          <input type="submit" style={{ display: "none" }} />
+        </form>
       </div>
 
-      <div className='calendar-month'>
+      <div className="calendar-month">
         {months &&
-          months.map((month, index, array) => {
-            const leftDaysPrevMonth = calculateLeftDaysFromPrevoiusMonth(month, index, array);
+          months.map((month, index, monthsArr) => {
+            const leftDaysPrevMonth = calculateLeftDaysFromPrevoiusMonth(month, index, monthsArr);
 
-            return <Month key={`${month.month}_${index}`} year={currentYear} monthData={month} daysPlaceholdersCount={leftDaysPrevMonth} />;
+            return (
+              <Month
+                key={`${month.month}_${index}`}
+                year={currentYear}
+                monthData={month}
+                daysPlaceholdersCount={leftDaysPrevMonth}
+              />
+            );
           })}
       </div>
     </div>
